7f9c74e9947fb0aa0e6fdd159a8337cb06a11d12
o_sapanyuk <oleg.sapanyuk@mindgeek.com>
2024-01-07 21:19:14 -0500

    Completed section 3.3

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
 
     another_function(5);
     print_labeled_measurement(5, 'h');
+    expression();
+    return_value();
 }
 
 fn another_function(x: i32) {
@@ -11,4 +13,31 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is {y}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(v: i32) -> i32 {
+    return v + 1; //semi-colon in this case is optional, since v+1 is our expression
+}
+
+fn return_value() {
+    let z = five();
+
+    println!("The value of z is: {z}");
+
+    let v = plus_one(z);
+
+    println!("The value of v is: {v}");
 }
