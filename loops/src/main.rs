6e4dab11669fb34cea8b0d7b08d252d09949a70c
osapanyuk <oleg.sapanyuk@gmail.com>
2024-01-09 21:06:50 -0500

    Completed theoretical section of 3.5

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+    loop_example();
+    nested_loop();
+    while_loop();
+    for_loop();
+    while_with_for();
+}
+
+fn loop_example() {
     let mut counter = 0;
 
     let result = loop {
@@ -10,4 +18,55 @@ fn main() {
     };
 
     println!("The result is {result}");
+}
+
+fn nested_loop() {
+    let mut count = 0;
+
+    'count_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'count_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("Liftoff!!");
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("The value is: {element}");
+    }
+}
+
+fn while_with_for() {
+    for number in (1..4).rev() { // have to reverse, 4..1 is not a thing
+        println!("{number}!");
+    }
+
+    println!("Liftoff!!");
 }
