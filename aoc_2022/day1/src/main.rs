a0a81cc3c352ddcb3f858bb378dce4dd7d32ee2f
osapanyuk <oleg.sapanyuk@gmail.com>
2024-02-06 08:06:43 -0500

    aoc_2022_1

--- aoc_2022/day1/src/main.rs
+++ aoc_2022/day1/src/main.rs
@@ -4,12 +4,20 @@ fn main() {
     let mut current_sum: u64 = 0;
     let mut highest_sum: u64 = 0;
     for line in fs::read_to_string("input.txt")
-        .unwrap().lines() {
-            if line.trim() == "" {
+        .unwrap()
+        .lines() {
+
+            let trimmed_line = line.trim();
+
+            if trimmed_line == "" {
                 current_sum = 0;
             } else {
-                let calorie: u64 = line.trim().parse().expect("Failed to convert to int");
+                let calorie: u64 = trimmed_line
+                    .parse()
+                    .expect("Failed to convert to int");
+
                 current_sum = current_sum + calorie;
+
                 if highest_sum < current_sum {
                     highest_sum = current_sum;
                 }
