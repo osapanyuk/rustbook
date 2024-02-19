62c72c7ef84e83e0bfb76e155094c11d2dd1f3fd
osapanyuk <oleg.sapanyuk@gmail.com>
2024-02-19 15:50:51 -0500

    Second part of day 1 2022

--- aoc_2022/day1/src/main.rs
+++ aoc_2022/day1/src/main.rs
@@ -2,7 +2,9 @@ use std::{fs, u64};
 
 fn main() {
     let mut current_sum: u64 = 0;
-    let mut highest_sum: u64 = 0;
+    let mut first_sum: u64 = 0;
+    let mut second_sum: u64 = 0;
+    let mut third_sum: u64 = 0;
     for line in fs::read_to_string("input.txt")
         .unwrap()
         .lines() {
@@ -18,11 +20,21 @@ fn main() {
 
                 current_sum = current_sum + calorie;
 
-                if highest_sum < current_sum {
-                    highest_sum = current_sum;
+                if first_sum < current_sum {
+                    third_sum = second_sum;
+                    second_sum = first_sum;
+                    first_sum = current_sum;
+                } else if second_sum < current_sum {
+                    third_sum = second_sum;
+                    second_sum = current_sum;
+                } else if third_sum < current_sum {
+                    third_sum = current_sum;
                 }
             }
     }
 
-    println!("{highest_sum}")
+    println!("First sum: {first_sum}");
+    println!("Second sum: {second_sum}");
+    println!("Third sum: {third_sum}");
+    println!("Final sum: {}", first_sum + second_sum + third_sum);
 }
